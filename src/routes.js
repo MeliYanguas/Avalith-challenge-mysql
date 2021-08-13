417fac881280c77ed8e1e08f60690c846b7f09ff
Rocio Yang <melinayanguas@gmail.com>
2021-08-12 21:19:02 -0300

    validation of complete fields and res status added

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.get('/books', (req, res) => {
     req.getConnection((err, connection) => {
-        if (err) return res.send(err);
+        if (err) return res.status(500).send(err);
 
         connection.query('SELECT * FROM books', (err, rows) => {
-            if (err) return res.send(err);
+            if (err) return res.status(500).send(err);
 
             res.json(rows);
             
@@ -16,43 +16,54 @@ router.get('/books', (req, res) => {
 
 router.get('/books/:id', (req, res) => {
     req.getConnection((err, connection) => {
-        if (err) return res.send(err);
+        if (err) return res.status(500).send(err);
 
         connection.query('SELECT * FROM books WHERE id = ?', [req.params.id], (err, rows) => {
-            if (err) return res.send(err);
-
+            if (err) return res.status(500).send(err);
             res.json(rows[0]);
         });
     });
 });
 
 router.post('/books', (req, res) => {
-    req.getConnection((err, connection) => {
-        if (err) return res.send(err);
-        connection.query('INSERT INTO books set ?', [req.body], (err, rows) => {
-            if (err) return res.send(err);
+    const {name, author, isbn} = req.body;
+    if (!(name && author && isbn)){
+        return res.status(400).send('Completar los campos')
+    } else {
+        req.getConnection((err, connection) => {
+            if (err) return res.status(500).send(err);
+            connection.query('INSERT INTO books set ?', [req.body], (err, rows) => {
+                if (err) return res.status(500).send(err);
 
-            res.send('El libro ha sido registrado');
+                res.send('El libro ha sido registrado');
+            });
         });
-    });
+    }
+    
 });
 
 router.put('/books/:id', (req, res) => {
-    req.getConnection((err, connection) => {
-        if (err) return res.send(err);
-        connection.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+    const {name, author, isbn} = req.body;
+    if(!(name && author && isbn)){
+        return res.status(400).send('Verifique los campos')
+    } else {
+        req.getConnection((err, connection) => {
             if (err) return res.send(err);
+            connection.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+                if (err) return res.status(500).send(err);
 
-            res.send('El libro ha sido actualizado');
+                res.send('El libro ha sido actualizado');
+            });
         });
-    });
+    }
+    
 });
 
 router.delete('/books/:id', (req, res) => {
     req.getConnection((err, connection) => {
         if (err) return res.send(err);
         connection.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows) => {
-            if (err) return res.send(err);
+            if (err) return res.status(500).send(err);
 
             res.send('El libro ha sido eliminado');
         });
